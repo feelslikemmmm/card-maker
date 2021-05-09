b30f3a05a6bd88a2110a4cdd3ffeb81b2b07a237
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-05-09 17:17:37 +0900

    card state setting

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './maker.module.css';
 import Header from '../header/header';
 import Footer from '../footer/footer';
@@ -6,6 +6,18 @@ import { useHistory } from 'react-router-dom';
 import Editor from '../editor/editor';
 import Preview from '../preview/preview';
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'feelslike',
+      company: 'kakao',
+      theme: 'light',
+      title: 'Software Engineer',
+      email: 'feelslikemmmm@gmail.com',
+      message: 'go for it',
+      fileName: 'feelslike',
+    },
+  ]);
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
