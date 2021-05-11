c2902eb65dae9d5b4b8af1d095a03a6143cb8bbb
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-05-11 18:33:20 +0900

    use firebase realtime database - sync cards

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -5,52 +5,31 @@ import Footer from '../footer/footer';
 import { useHistory } from 'react-router-dom';
 import Editor from '../editor/editor';
 import Preview from '../preview/preview';
-const Maker = ({ FileInput, authService }) => {
-  const [cards, setCards] = useState({
-    '1': {
-      id: '1',
-      name: 'feelslike',
-      company: 'kakao',
-      theme: 'dark',
-      title: 'Software Engineer',
-      email: 'feelslikemmmm@gmail.com',
-      message: 'go for it',
-      fileName: 'feelslike',
-      fileURL: null,
-    },
-    '2': {
-      id: '2',
-      name: 'feelslike2',
-      company: 'kakao',
-      theme: 'light',
-      title: 'Software Engineer',
-      email: 'feelslikemmmm@gmail.com',
-      message: 'go for it',
-      fileName: 'feelslike',
-      fileURL: null,
-    },
-    '3': {
-      id: '3',
-      name: 'feelslike3',
-      company: 'kakao',
-      theme: 'colorful',
-      title: 'Software Engineer',
-      email: 'feelslikemmmm@gmail.com',
-      message: 'go for it',
-      fileName: 'feelslike',
-      fileURL: null,
-    }
-  });
-
-
+const Maker = ({ FileInput, authService, cardRepository }) => {
   const history = useHistory();
+  const historyState = history?.location?.state;
+  const [cards, setCards] = useState({});
+  const [userId, setUserId] = useState(historyState && historyState.id);
+
   const onLogout = () => {
     authService.logout();
   };
 
   useEffect(() => {
+    if (!userId) {
+      return;
+    }
+    const stopSync = cardRepository.syncCards(userId, cards => {
+      setCards(cards);
+    });
+    return () => stopSync();
+  }, [userId]);
+
+  useEffect(() => {
     authService.onAuthChange(user => {
-      if (!user) {
+      if (user) {
+        setUserId(user.uid);
+      } else {
         history.push('/');
       }
     });
@@ -63,7 +42,8 @@ const Maker = ({ FileInput, authService }) => {
       updated[card.id] = card;
       return updated;
     });
-  }
+    cardRepository.saveCard(userId, card);
+  };
 
   const deleteCard = (card) => {
     setCards(cards => {
@@ -71,7 +51,9 @@ const Maker = ({ FileInput, authService }) => {
       delete updated[card.id];
       return updated;
     });
-  }
+    cardRepository.removeCard(userId, card);
+  };
+
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
